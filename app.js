fbd5da159315465b633b1f7b37762a3d5dabbe5d
lexur <aliahmadi.github@gmail.com>
2025-01-09 00:15:44 +0330

    -

--- app.js
+++ app.js
@@ -5,6 +5,12 @@ const app = express();
 const server = http.createServer(app);
 
 
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/public/index.html");
+});
+
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
     console.log(`listening on ${PORT}`);
